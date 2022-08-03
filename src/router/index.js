b91af95398eec48c57a007d76751ea2cf1340864
martynhorslen <2880060+MartynHorslen@users.noreply.github.com>
2022-08-03 11:51:04 +0100

    Redirect unrecognised paths back to News view.

--- src/router/index.js
+++ src/router/index.js
@@ -20,19 +20,17 @@ const router = createRouter({
     {
       path: "/fixtures",
       name: "Fixtures",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/FixturesView.vue"),
     },
     {
       path: "/results",
       name: "Results",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/ResultsView.vue"),
     },
+    {
+      path: "/:catchAll(.*)", // Unrecognized path automatically returns home/news page.
+      component: NewsView
+    }
   ],
   linkActiveClass: "nav-active",
   linkExactActiveClass: ""
